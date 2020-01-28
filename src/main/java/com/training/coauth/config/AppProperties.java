c9bd252d116ebbad6f6235c41bda55a41b0d2808
Paras Tripathi <parastripathilearner@gmail.com>
2020-01-28 22:30:35 +0530

    Added Application properties configuration

---
+++ src/main/java/com/training/coauth/config/AppProperties.java
@@ -0,0 +1,36 @@
+package com.training.coauth.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+    private final Auth auth = new Auth();
+
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public static class Auth {
+        private String tokenSecret;
+        private long tokenExpirationMsec;
+
+        public String getTokenSecret() {
+            return tokenSecret;
+        }
+
+        public void setTokenSecret(String tokenSecret) {
+            this.tokenSecret = tokenSecret;
+        }
+
+        public long getTokenExpirationMsec() {
+            return tokenExpirationMsec;
+        }
+
+        public void setTokenExpirationMsec(long tokenExpirationMsec) {
+            this.tokenExpirationMsec = tokenExpirationMsec;
+        }
+    }
+
+
+}
