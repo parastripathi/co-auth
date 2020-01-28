356ea86e8343617e3f9f9a4ea83424cb5882d6e8
Paras Tripathi <parastripathilearner@gmail.com>
2020-01-28 22:36:51 +0530

    Handle CORS

--- src/main/java/com/training/coauth/CoAuthApplication.java
+++ src/main/java/com/training/coauth/CoAuthApplication.java
@@ -1,9 +1,12 @@
 package com.training.coauth;
 
+import com.training.coauth.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class CoAuthApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/training/coauth/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.training.coauth.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
